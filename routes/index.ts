5161c6c73b0f40d9dc6c8beb759623c98eca5912
nitipon <nitipon.apaisri@gmail.com>
2023-08-04 22:10:20 +0200

    Changed import style for controller on routes/index.ts to look better

--- routes/index.ts
+++ routes/index.ts
@@ -1,16 +1,16 @@
 import { Router } from "express";
 
-import { getUsers, getUser, registerUser, signIn, updateUserName, deleteUser, updateUserEmail, updateUserPassword, createResetPasswordLink } from "../controllers/userController";
+import * as userController from "../controllers/userController";
 import { auth } from "../middlewares/auth";
 const router = Router();
 
-router.get("/users", auth, getUsers);
-router.get("/user/:id", auth, getUser);
-router.post("/user/signup", registerUser);
-router.post("/user/signin", signIn);
-router.patch("/user/:id", auth, updateUserName);
-router.patch("/user/:id/email", auth, updateUserEmail);
-router.patch("/user/:id/changePassword", auth, updateUserPassword);
-router.post("/get-reset-password-link", createResetPasswordLink);
-router.delete("/user/:id", auth, deleteUser);
+router.get("/users", auth, userController.getUsers);
+router.get("/user/:id", auth, userController.getUser);
+router.post("/user/signup", userController.registerUser);
+router.post("/user/signin", userController.signIn);
+router.patch("/user/:id", auth, userController.updateUserName);
+router.patch("/user/:id/email", auth, userController.updateUserEmail);
+router.patch("/user/:id/changePassword", auth, userController.updateUserPassword);
+router.post("/get-reset-password-link", userController.createResetPasswordLink);
+router.delete("/user/:id", auth, userController.deleteUser);
 export default router;
