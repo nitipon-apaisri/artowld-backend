554697a5aee7065f46ee164cefb917b25f687b06
nitipon <nitipon.apaisri@gmail.com>
2023-08-06 19:11:39 +0200

    Fixed route for upload file

--- controllers/productController.ts
+++ controllers/productController.ts
@@ -1,7 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import bcrypt from "bcrypt";
 import { v4 as uuidv4 } from "uuid";
-
 import { productType } from "../types/productTypes";
 import productModel from "../models/productModel";
 import path from "path";
@@ -34,7 +32,7 @@ const registerProduct = async (req: Request, res: Response, next: NextFunction) 
 
 const uploadFile = async (req: Request, res: Response, next: NextFunction) => {
     const file = req.file;
-    const readFile = fs.readFileSync(path.join("./uploads/" + file?.filename));
+    const readFile = fs.readFileSync(path.join("./uploads/" + file?.filename)); // read the file from uploads folder
     const imgObj = {
         _id: uuidv4(),
         name: file?.filename,
@@ -42,9 +40,9 @@ const uploadFile = async (req: Request, res: Response, next: NextFunction) => {
         contentType: file?.mimetype,
     };
     try {
-        await imageModel.create(imgObj);
+        await imageModel.create(imgObj); // save the image to the db
         setTimeout(() => {
-            fs.unlinkSync(path.join("./uploads/" + file?.filename));
+            fs.unlinkSync(path.join("./uploads/" + file?.filename)); // delete the file from uploads folder
         }, 1000);
         res.status(200).json({ message: "Image uploaded successfully" });
     } catch (error) {
@@ -54,20 +52,15 @@ const uploadFile = async (req: Request, res: Response, next: NextFunction) => {
 };
 
 const retrieveImg = async (req: Request, res: Response, next: NextFunction) => {
-    const { id } = req.params;
+    const { id } = req.params; // get the id from the request
     try {
         await imageModel.findById(id).then((img: any) => {
-            const buffer = Buffer.from(img?.data, "base64");
-            res.writeHead(200, { "Content-Type": img?.contentType, "Content-Length": img?.data.length });
-            res.end(buffer);
-            // res.set("Content-Type", "text/html");
-            // res.send(`<img src="https://pbs.twimg.com/media/F21OcvXbIAAc6JQ?format=jpg&name=small"/>`);
-            // res.send(`
-            // <img src="data:${img?.contentType};base64,${buffer}" alt="img"/>
-            // `);
+            const buffer = Buffer.from(img?.data, "base64"); // decode the base64 from the db
+            res.writeHead(200, { "Content-Type": img?.contentType, "Content-Length": img?.data.length }); // write the header of the response
+            res.end(buffer); // send the buffer to the response
         });
     } catch (error) {
-        const err = error as Error;
+        const err = error as Error; // handle the error
         res.status(500).json({ message: err.message });
     }
 };
